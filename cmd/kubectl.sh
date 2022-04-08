acb227eb86f6e5f716c24552bd0a2f9179d26048
Isabella Coimbra <isabella.coimbra@g.globo>
2022-04-08 14:52:32 -0300

    Add clusterip, nodeport and load balancer

--- cmd/kubectl.sh
+++ cmd/kubectl.sh
@@ -12,6 +12,10 @@ kubectl descibre nginx-pod
 
 # Create a pod with file
 kubectl apply -f first-pod.yaml
+kubectl apply -f pod-nginx.yaml
+kubectl apply -f svc-pod.yaml
+kubectl apply -f load-balancer.yaml
+
 
 # Delete pod
 # kubectl delete pod nginx-pod
@@ -19,3 +23,6 @@ kubectl apply -f first-pod.yaml
 
 # To access the terminal in the container on the pod
 kubectl exec -it first-pod-declarative -- bash
+
+# The minikube dashboard command, which opens a new tab in our web browser, displaying the Kubernetes Dashboard:
+minikube dashboard 
